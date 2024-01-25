6458ab4170c872ea6e1ae0e8839ae6d6bb59f6c7
Aaryamann Singh <aaryamann.singh@aaryamann.singh-C02FT0ABMD6V>
2024-01-25 13:02:49 +0530

    add storage persistence to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,26 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface UserState {
-    username: string | null,
-    setUser: (name: string) => void,
-    removeUser: () => void
-  }
 
-const useStore = create<UserState>((set) => ({
-  username: null,
-  setUser: (name) => set({ username: name}),
-  removeUser: () => set({username: null})
-}));
+  username: string | null;
+  setUser: (name: string) => void;
+  removeUser: () => void;
+}
+
+const useStore = create<UserState>()(
+  persist(
+    (set, get) => ({
+      username: null,
+      setUser: (name) => set({ username: name }),
+      removeUser: () => set({ username: null }),
+    }),
+    {
+      name: "user",
+      storage: createJSONStorage(()=> sessionStorage),
+    }
+  )
+);
+
 
 export default useStore;
