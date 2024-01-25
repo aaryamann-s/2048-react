32d88ac8b118e3e212f48c1d379b4d7ffc1e5f73
Aaryamann Singh <aaryamann.singh@aaryamann.singh-C02FT0ABMD6V>
2024-01-25 13:50:21 +0530

    fix routes and header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-import dynamic from "next/dynamic";
-import "../src/styles/App.css";
-import "../src/styles/tile-colors.css";
-
-const GameBoard = dynamic(() => import("../src/components/GameBoard"), {
-  ssr: false,
-});
-
 export default function Page() {
   return (
     <div
@@ -16,7 +8,7 @@ export default function Page() {
         gap: 8,
       }}
     >
-      <GameBoard />
+     HP
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,39 +3,48 @@
 import * as React from "react";
 
 import { useRouter, usePathname } from "next/navigation";
+import Link from "next/link";
 import { useStyletron } from "baseui";
 import { Layer } from "baseui/layer";
 import { ChevronDown, Delete, Overflow, Upload } from "baseui/icon";
 import { AppNavBar, setItemActive, NavItem } from "baseui/app-nav-bar";
 import useStore from "../store/store";
 
+
+const MENU_ITEMS = [
+  {
+    icon: Upload,
+    label: "Game",
+    info: { hideWhenLoggedIn: false,link: "/game" },
+  },
+  {
+    icon: Upload,
+    label: "Leaderboard",
+    info: { hideWhenLoggedIn: false,link: "/leaderboard" },
+  },
+  {
+    icon: Upload,
+    label: "Login",
+    info: { hideWhenLoggedIn: true, link: "/login" },
+  },
+];
+
 export default function Header() {
   const router = useRouter();
   const pathname = usePathname();
   const username = useStore((state) => state.username);
   const removeUser = useStore((state) => state.removeUser);
+  const isLoggedIn = username !== null;
   const [css] = useStyletron();
 
-  const [mainItems, setMainItems] = React.useState<NavItem[]>([
-    {
-      icon: Upload,
-      label: "Game",
-      info: { link: "/" },
-      active: pathname == "/",
-    },
-    {
-      icon: Upload,
-      label: "User",
-      info: { link: "/user" },
-      active: pathname == "/user",
-    },
-  ]);
+
+  const [mainItems, setMainItems] = React.useState<NavItem[]>(MENU_ITEMS.map(item => {
+    return {
+      ...item,
+      active: pathname === item.info.link
+    }
+  }));
   const userItems = [
-    {
-      icon: Overflow,
-      label: "Profile",
-      info: { action: () => router.push("/profile") },
-    },
     { icon: Overflow, label: "Logout", info: { action: () => removeUser() } },
   ];
 
@@ -56,10 +65,21 @@ export default function Header() {
         })}
       >
         <AppNavBar
-          title="2048er"
-          mainItems={mainItems}
+          title={
+            <Link
+              onClick={() => {
+                console.log(MENU_ITEMS)
+                setMainItems(MENU_ITEMS)}
+              }
+              style={{ textDecoration: "none", color: "white" }}
+              href="/"
+            >
+              2048er
+            </Link>
+          }
+          mainItems={isLoggedIn ? mainItems.filter(item => item.info?.hideWhenLoggedIn === false) : mainItems}
           onMainItemSelect={handleMainItemSelect}
-          userItems={username ? userItems : []}
+          userItems={isLoggedIn ? userItems : []}
           onUserItemSelect={(item: NavItem) => {
             item.info?.action();
           }}
